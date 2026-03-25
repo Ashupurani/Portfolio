1cf36a0bf7cda329cd0541ca091c3166be5fc193
Ashupurani <152039976+Ashupurani@users.noreply.github.com>
2026-03-25 15:27:34 -0400

    Delete script.js
    Remove unnecessary config file

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('year').textContent = new Date().getFullYear();
-
-// Optional: Google Analytics placeholder
-// window.dataLayer = window.dataLayer || [];
-// function gtag(){dataLayer.push(arguments);}
-// gtag('js', new Date());
-// gtag('config', 'G-XXXXXXXXXX');
